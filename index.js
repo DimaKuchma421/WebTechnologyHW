c7f622918170f32b7b7050f573de1fe8213e5451
Dima Kuchmambetov <dima2006421@gmail.com>
2024-10-05 21:09:04 +0300

    header upgrade, new web-pages, style changes

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/ribbon", (req, res) => {
+  res.render("ribbon.ejs");
+});
+app.get("/friends", (req, res) => {
+  res.render("friends.ejs");
+});
+app.get("/account", (req, res) => {
+  res.render("account.ejs");
+});
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
